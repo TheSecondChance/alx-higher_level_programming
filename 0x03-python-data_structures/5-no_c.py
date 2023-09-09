49815fdccbc46d3521ce05a6dad86e2456ae95c2
eyasu <mesakmegerem@gmail.com>
2023-09-09 14:44:29 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    letter1 = 'c'
-    letter2 = 'C'
-    new = my_string.replace(letter1, '').replace(letter2, '')
+    new = my_string.translate({ord(i): None for i in 'cC'})
     return new
