eaf5dce4d1575ff5141a47dce3d89c1bb29a2919
eyasu <mesakmegerem@gmail.com>
2023-09-10 14:12:57 -0700

    7-add_tuple.py modify style

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,6 +5,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
     b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
-    
+
     result_tuple = (a1 + b1, a2 + b2)
     return result_tuple
