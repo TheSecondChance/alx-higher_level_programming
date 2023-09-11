ea3f84a8dce1e228c7bca588b60ea35b593d3934
TheSecondChance <132612647+TheSecondChance@users.noreply.github.com>
2023-09-11 18:42:11 +0400

    lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -6,13 +6,13 @@
 #include <unistd.h>
 
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * for Holberton project
- *        */
+ * truct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
 		int n;
