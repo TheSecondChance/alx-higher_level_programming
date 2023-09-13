f19703808d1c5e48cb31df776d13035ab053b8a9
eyasu <mesakmegerem@gmail.com>
2023-09-13 05:52:11 -0700

    modify

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    n_list = my_list.copy()
     for i in range(len(my_list)):
-        new_list = my_list.copy()
-        if new_list[i] == search:
-            new_list[i] = replace
-    return new_list
+        if n_list[i] == search:
+            n_list[i] = replace
+    return n_list
