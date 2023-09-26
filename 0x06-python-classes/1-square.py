3d6cacaf09c154974bbcf03849c8bd6b17c4cac2
eyasu <mesakmegerem@gmail.com>
2023-09-26 12:39:46 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This for 1-square
+Define a class Square"""
+
+class Square:
+    """This for Represent a square
+    """
+
+    def __init__(self, size):
+        """This for Initialize method new Square"""
+        self.__size = size
