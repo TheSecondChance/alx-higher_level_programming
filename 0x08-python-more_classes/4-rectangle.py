c46b9854a4cd6e8e10a1b7d5ad8b504bd22526f8
eyasu <mesakmegerem@gmail.com>
2023-10-02 06:10:16 -0700

    4-rectangle modifiy

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
-    
+
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         return f"Rectangle({self.__width}, {self.__height})"
