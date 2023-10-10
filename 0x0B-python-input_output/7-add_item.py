54a87d8035b0db3c5189d61533d0d83c57d69001
eyasu <mesakmegerem@gmail.com>
2023-10-10 11:41:08 -0700

    modifiy all code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,26 +3,15 @@
     list, and then save them to a file:
 """
 import sys
-import os
-save_to_json = __import__("5-save_to_json_file").save_to_json_file
-load_from_json = __import__("6-load_from_json_file").load_from_json_file
-
-
-def add_item(args, filename):
-    """Adds items JSON file.
-        Args:
-            args: list of items to add.
-            filename: path to the JSON file.
-    """
-    if (os.path.exists(filename)):
-        content = load_from_json(filename)
-    else:
-        content = []
-    content.extend(args)
-    save_to_json(content, filename)
-
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    filename = "add_item.json"
-    add_item(args, filename)
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
