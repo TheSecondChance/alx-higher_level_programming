76d8e0dfcfa3d6369b9debf11f80cbd49b44c5de
eyasu <mesakmegerem@gmail.com>
2023-10-10 07:56:07 -0700

    modfiy indentation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -9,6 +9,4 @@ def to_json_string(my_obj):
         Args:
             my_obj: str representation
         Return: the JSON representation"""
-
-        return json.dumps(my_obj)
-
+    return json.dumps(my_obj)
