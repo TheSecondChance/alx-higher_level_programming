5ce8f867cf312d7fb912e4b79cff08eb83bbff2b
eyasu <mesakmegerem@gmail.com>
2023-10-10 12:28:39 -0700

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class Student that defines a studen"""
+
+
+class Student:
+    """create student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        special method initialize
+        """
+        self.name = first_name
+        self.lname = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrive the dict reprenting"""
+        return self.__dict__
