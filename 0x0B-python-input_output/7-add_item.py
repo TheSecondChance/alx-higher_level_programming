964bbbf3b54117baf03cb7eaabc6c213d372d392
eyasu <mesakmegerem@gmail.com>
2023-10-10 10:21:46 -0700

    remove content

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,7 +13,7 @@ def add_item(args, filename):
         args: list of items to add.
         filename: path to the JSON file.
     """
-    content = load_from_json(filename) if os.path.exists(filename) else content = []
+    content = load_from_json(filename) if os.path.exists(filename) else []
     content.extend(args)
     save_to_json(content, filename)
 
