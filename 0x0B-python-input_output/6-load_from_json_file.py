ceb8b2b1a8097625b9392231f55e68fa199c7c1b
eyasu <mesakmegerem@gmail.com>
2023-10-10 09:49:05 -0700

    avoid my_obj

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """creates an Object from a “JSON file"""
     with open(filename, encoding="utf-8") as file:
-        my_obj = json.load(file)
+        json.load(file) 
