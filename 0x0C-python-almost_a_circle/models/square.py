ea4cefc9444e99cc4fe6c30202d99047054b2148
eyasu <mesakmegerem@gmail.com>
2023-10-13 18:02:24 -0700

    square.py for task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python3
-"""Import from models dirctory"""
+"Import from models directory"
 from models.rectangle import Rectangle
 
+
+class Square(Rectangle):
+    """Square class
+
+    Args:
+        Rectangle (class): inherit
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize Square
+
+        Args:
+            size (int): size of square
+            x (int): Defaults to 0.
+            y (int): Defaults to 0.
+            id (int): id Base
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String representation of the square
+
+        Returns:
+            str: string represent
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
