d8eabd638b811d6ded46402ea9fe908bb587717c
eyasu <mesakmegerem@gmail.com>
2023-10-15 06:38:05 -0700

    base.py for task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries (list): list of dictionaries
+
+        Returns:
+            list: Return the JSON serialization
+        """
+        if list_dictionaries is None or []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
