d023a35aaf9138fff7a116808bb3f8d073bfd8da
eyasu <mesakmegerem@gmail.com>
2023-10-15 08:47:50 -0700

    base.py for task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,3 +48,17 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 file.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """decoding a JSON string
+
+        Args:
+            json_string (str):  JSON str representation of a list of dicts
+
+        Returns:
+            list represented by json_string
+        """
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
