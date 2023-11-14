44197109c486253570aa4cd049e6f6e901e661f6
eyasu <mesakmegerem@gmail.com>
2023-11-14 14:03:48 -0800

    missed s

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- script that displays the average temperature
-SELECT city, AVG(value) as avg_temp FROM temperature
+SELECT city, AVG(value) as avg_temp FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
