f79bd9ba2e3b6b4b700d6ef9f4ba0135e47eee16
eyasu <mesakmegerem@gmail.com>
2023-11-15 06:38:33 -0800

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,7 @@
 -- script that creates the database
-CREATE TABLE IF NOT EXISTS unique_id (
-	id INT DEFAULT 1 UNIQUE,
-	name VARCHAR(256));
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL, 
+	PRIMARY KEY(id));
