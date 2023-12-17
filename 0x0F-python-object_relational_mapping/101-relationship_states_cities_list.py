998e856792f7e5fb7667b8a8a3d22b2c3f4a3dce
eyasu <mesakmegerem@gmail.com>
2023-12-17 12:28:34 -0800

    Fix 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -18,8 +18,10 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     geaz = sessionmaker(bind=engine)
     session = geaz()
-    for state in session.query(State):
-        print("{:d}: {}".format(state.id, state.name))
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
-            print("    {:d}: {}".format(city.id, city.name))
+            print('\t{}: {}'.format(city.id, city.name))
+
     session.close()
