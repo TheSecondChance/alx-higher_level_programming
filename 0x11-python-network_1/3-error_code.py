75a8639c4f64e7ab2a9356259d87ec8501f05a15
eyasu <mesakmegerem@gmail.com>
2024-01-06 07:24:15 -0800

    Add 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request and displays 
+the body of the response
+"""
+from urllib import request, error
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            akal = res.read()
+            print(akal.decode('utf-8'))
+    except error.HTTPError as chegr:
+        print('Error code: {}'.format(chegr.code))
