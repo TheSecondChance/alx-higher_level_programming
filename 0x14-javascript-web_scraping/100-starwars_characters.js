ed4d09abd7505391eb518a902a57ed568712b7a9
TheSecondChance <mesakmegerem@gmail.com>
2024-01-30 09:12:29 -0800

    Add 100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const reques = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+reques.get(url + id, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  
+  const data = JSON.parse(body);
+  const dataChar = data.characters;
+
+  for (const i of dataChar) {
+    reques.get(i, function (error, response, bodyOne) {
+      if (error) {
+        console.log(error);
+      }
+      const dataOne = JSON.parse(bodyOne);
+      console.log(dataOne.name);
+    });
+  }
+});
