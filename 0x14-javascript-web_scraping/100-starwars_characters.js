1d01fb4f3e2a4955eab62b81a7df9875e3b52754
TheSecondChance <mesakmegerem@gmail.com>
2024-01-30 09:13:29 -0800

    Fix 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,8 +6,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/';
 reques.get(url + id, function (error, response, body) {
   if (error) {
     console.log(error);
-  }
-  
+  }  
   const data = JSON.parse(body);
   const dataChar = data.characters;
 
