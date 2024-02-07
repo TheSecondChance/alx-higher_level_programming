4c9853e93efe5d921223bf149a7d49854883f94f
TheSecondChance <mesakmegerem@gmail.com>
2024-02-07 08:23:00 -0800

    Add 9-script

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $.get(
+    'https://fourtonfish.com/hellosalut/?lang=fr',
+    function (res) {
+      $('DIV#hello').text(res.hello);
+    },
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+  );
+});
