9937162e9a3c1d196490cfabc6e52d8591d35394
TheSecondChance <mesakmegerem@gmail.com>
2024-02-07 01:51:26 -0800

    Add 4-script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,13 @@
+$('DIV#toggle_header').click(
+  function () {
+    const header = $('header');
+    if (header.hasClass('red')) {
+      header.removeClass('red');
+      header.addClass('green');
+    } else {
+      header.removeClass('green');
+      header.addClass('red');
+    }
+  }
+);
+
